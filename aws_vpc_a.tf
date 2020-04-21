8d46da009efda9c8b59b474c5670c9ce47e12425
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2020-04-21 00:46:49 -0300

    create route table for vpc a

--- aws_vpc_a.tf
+++ aws_vpc_a.tf
@@ -4,3 +4,15 @@ resource "aws_vpc" "a" {
     name = "network-a"
   }
 }
+
+resource "aws_route_table" "a" {
+  vpc_id = aws_vpc.a.id
+  tags = {
+    name = "network-a"
+  }
+}
+
+resource "aws_main_route_table_association" "a" {
+  vpc_id         = aws_vpc.a.id
+  route_table_id = aws_route_table.a.id
+}
