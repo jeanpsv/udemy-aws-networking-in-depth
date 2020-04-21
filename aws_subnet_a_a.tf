4db9387d5e741e9211449424469899d332d0f88d
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2020-04-21 16:19:29 -0300

    create subnet a

--- aws_subnet_a_a.tf
+++ aws_subnet_a_a.tf
@@ -6,3 +6,16 @@ resource "aws_subnet" "a_a" {
     subnet  = "a"
   }
 }
+
+resource "aws_route_table" "a_a" {
+  vpc_id = aws_vpc.a.id
+  tags = {
+    network = "a"
+    subnet  = "a"
+  }
+}
+
+resource "aws_route_table_association" "a_a" {
+  subnet_id      = aws_subnet.a_a.id
+  route_table_id = aws_route_table.a_a.id
+}
